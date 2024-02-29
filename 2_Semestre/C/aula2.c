9184f1fc9ecfd80ea294e1a2eb05f806389ec0fe
Caio Marques <caiombr@gmail.com>
2024-02-28 23:03:42 -0300

    Codigo da aula 2 de C

---
+++ 2_Semestre/C/aula2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.1415
+
+// // 1º
+// int main(){
+//     float imc, peso, altura;
+//     system("cls");
+//     printf("Digite o peso e a altura:");
+//     scanf("%f %f",&peso,&altura);
+//     imc = peso/pow(altura,2);
+//     printf("O resultado do imc deu: %.2f",imc);
+//     return 0;
+// }
+
+// // 2º
+// int main(){
+//     float f, c;
+//     system("cls");
+//     printf("Digite a temperatura em F:");
+//     scanf("%f",&f);
+//     c = (f - 32)* 5/9;
+//     printf("Celcius = %.2f",c);
+//     return 0;
+// }
+
+// // 3º
+// int main(){
+//     float r, a, p;
+//     system("cls");
+//     printf("Digite o Raio:");
+//     scanf("%f",&r);
+//     a = PI * pow(r,2);
+//     p = 2*PI*r;
+//     printf("Area = %.2f \nPerimetro = %.2f",a,p);
+//     return 0;
+// }
+
+// // 4º
+// int main(){
+//     char letra;
+//     system("cls");
+//     printf("Digite uma letra: ");
+//     scanf("%c",&letra);
+
+//     printf("Valor em octal: %o\n",letra);
+//     printf("Valor em decimal: %d\n",letra);
+//     printf("Valor em hexadecimal: %x\n",letra);
+
+//     return 0;
+// }
