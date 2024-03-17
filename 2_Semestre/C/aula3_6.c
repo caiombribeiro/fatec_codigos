048780c4b926ac14d14e28d271a9cab4be5bcb2d
Caio Marques <caiombr@gmail.com>
2024-03-17 16:43:25 -0300

    correção de ume xercicio

--- 2_Semestre/C/aula3_6.c
+++ 2_Semestre/C/aula3_6.c
@@ -1,8 +1,45 @@
 // Dados último dígito da placa de um carro, informe o dia do seu rodízio. Sugestão: utilize switch/ case.
 
 #include <stdio.h>
+#include <string.h>
 
-int main() 
-{
-    
+int main() {
+    char placa[8];
+    char ultimoDigito;
+
+    system("cls");
+
+    printf("Digite a placa do carro (formato: 0000-ABC): ");
+    scanf("%s", placa);
+
+    // Obtendo o último caractere (dígito) da placa
+    ultimoDigito = placa[3];
+    printf("\n%c\n", ultimoDigito);
+
+    switch (ultimoDigito) {
+        case '1':
+        case '2':
+            printf("SEGUNDA-FEIRA.\n");
+            break;
+        case '3':
+        case '4':
+            printf("TERÇA-FEIRA.\n");
+            break;
+        case '5':
+        case '6':
+            printf("QUARTA-FEIRA.\n");
+            break;
+        case '7':
+        case '8':
+            printf("QUINTA-FEIRA.\n");
+            break;
+        case '9':
+        case '0':
+            printf("SEXTA-FEIRA.\n");
+            break;
+        default:
+            printf("Número inválido.\n");
+    }
+
+    return 0;
 }
